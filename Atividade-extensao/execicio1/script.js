2d761ec6f565bbe226145ec9d079ff1b5a654a0b
Marcos Michel Luciano <56523274+michelluciano@users.noreply.github.com>
2021-09-04 12:05:26 -0300

    aula 04-09-2021
    aula de javascript

---
+++ Atividade-extensao/execicio1/script.js
@@ -0,0 +1,17 @@
+//pegar o body
+const body = document.querySelector('body');
+//pegar os botões
+const btns = document.querySelectorAll('button');
+//adicionar o evento
+btns[0].addEventListener('click', function(e){
+   body.style="background-color:red;";
+})
+btns[1].addEventListener('click', function(e){
+    body.style="background-color:yellow;";
+})
+btns[2].addEventListener('click', function(e){
+    body.style="background-color:blue;";
+ })
+ btns[3].addEventListener('click', function(e){
+    body.style="background-color:green;";
+ })
